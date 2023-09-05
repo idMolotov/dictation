5677cc2f433e7cfd9cb52053cba6e6097f322f28
Alexey Tsoi <atsoi@xiag.ch>
2023-09-05 14:32:14 +0700

    split code by functionality

---
+++ js/main.js
@@ -0,0 +1,47 @@
+console.log('classLessonsList', classLessonsList);
+const sourceWords = classLessonsList['class_3']['lesson_15'];
+
+// const fromLang = 'en';
+// const toLang = 'ru';
+const fromLang = 'ru';
+const toLang = 'en';
+
+function getRandomWordData() {
+    return sourceWords[Math.round(Math.random() * (sourceWords.length - 1))];
+}
+
+function clearForm() {
+    ['ru', 'en'].forEach((sourceLang) => {
+        const fromId = 'translation_' + sourceLang;
+        document.getElementById(fromId).innerText = '';
+    })
+}
+
+function getRandomListElement(sourceList) {
+    return sourceList[Math.round(Math.random() * (sourceList.length - 1))];
+}
+
+function initForm() {
+    const fromId = 'translation_' + fromLang;
+    const toId = 'translation_' + toLang;
+    console.log('fromId', fromId);
+
+    const wordData = getRandomWordData();
+    // document.getElementById(fromId).innerText = wordData[sourceLang];
+    document.getElementById(fromId).innerHTML = wordData[fromLang].join(',');
+    document.getElementById(toId).innerHTML = '?';
+
+    document.getElementById('block_action').value = 'show translation';
+    document.getElementById('block_action').onclick = () => {
+        document.getElementById('block_action').value = 'next word';
+        document.getElementById(toId).innerHTML = getRandomListElement(wordData[toLang]);
+        document.getElementById(toId).innerHTML = wordData[toLang].join(',');
+        if (toLang === 'en') speak(wordData[toLang][0]);
+        document.getElementById('block_action').onclick = () => {
+            initForm();
+        };
+    };
+    if (fromLang === 'en') speak(document.getElementById(fromId).innerHTML);
+}
+
+initForm();
