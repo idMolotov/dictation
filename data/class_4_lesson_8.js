7549e730a1123ca183d5f494e114f9a2e5fe9ebe
idMolotov <idMolotov@users.noreply.github.com>
2023-10-05 19:14:45 +0700

    Update class_4_lesson_8.js

--- data/class_4_lesson_8.js
+++ data/class_4_lesson_8.js
@@ -1,1 +1,12 @@
-
+const sourceWords_class_4_lesson_8 = [
+        {
+            'en': ['fat'],
+            'ru': ['толстый'],
+        },
+    ];
+/**
+ * @global classLessonsList
+ */
+classLessonsList['class_4'] = classLessonsList['class_4'] || {};
+classLessonsList['class_4']['lesson_8'] = classLessonsList['class_4']['lesson_8'] || {};
+classLessonsList['class_4']['lesson_8'] = sourceWords_class_4_lesson_1;
