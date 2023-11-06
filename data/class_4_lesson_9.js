c6b9319f7c08d4939ae2ac4ed90ae04908d1d66d
karijoy <150017505+karijoy@users.noreply.github.com>
2023-11-06 17:40:27 +0700

    Create class_4_lesson_9.js
    init

---
+++ data/class_4_lesson_9.js
@@ -0,0 +1,100 @@
+const sourceWords_class_4_lesson_9 = [
+        {
+            'en': ['post office'],
+            'ru': ['почта'],
+        },
+        {
+            'en': ["greengrocer's"],
+            'ru': ['овощной'],
+        },
+        {
+            'en': ['bank'],
+            'ru': ['банк'],
+        },
+        {
+            'en': ['hotel'],
+            'ru': ['отель'],
+        },
+        {
+            'en': ["baker's"],
+            'ru': ['булочная, хлебный'],
+        },
+        {
+            'en': ["butcher's"],
+            'ru': ['мясник, мясная лавка'],
+        },
+        {
+            'en': ['police station'],
+            'ru': ['полицейский участок'],
+        },
+        {
+            'en': ['supermarket'],
+            'ru': ['супермаркет'],
+        },
+        {
+            'en': ['hospital'],
+            'ru': ['больница'],
+        },
+        {
+            'en': ['bus stop'],
+            'ru': ['автобусная остановка'],
+        },
+        {
+            'en': ['cafe'],
+            'ru': ['кафэ'],
+        },
+        {
+            'en': ['cinema'],
+            'ru': ['кинотеатр'],
+        },
+        {
+            'en': ['jumper'],
+            'ru': ['свитер'],
+        },
+        {
+            'en': ['jeans'],
+            'ru': ['джинсы'],
+        },
+        {
+            'en': ['scarf'],
+            'ru': ['шарф'],
+        },
+        {
+            'en': ['cap'],
+            'ru': ['кепка'],
+        },
+        {
+            'en': ['coat'],
+            'ru': ['пальто'],
+        },
+        {
+            'en': ['gloves'],
+            'ru': ['перчатки'],
+        },
+        {
+            'en': ['pyjamas'],
+            'ru': ['пижама'],
+        },
+        {
+            'en': ['slippers'],
+            'ru': ['тапочки'],
+        },
+        {
+            'en': ['one penny'],
+            'ru': ['один пенни'],
+        },
+        {
+            'en': ['two pence'],
+            'ru': ['два пенса'],
+        },
+        {
+            'en': ['one pound'],
+            'ru': ['один фунт'],
+        },
+    ];
+/**
+ * @global classLessonsList
+ */
+classLessonsList['class_4'] = classLessonsList['class_4'] || {};
+classLessonsList['class_4']['lesson_9'] = classLessonsList['class_4']['lesson_9'] || {};
+classLessonsList['class_4']['lesson_9'] = sourceWords_class_4_lesson_9;
