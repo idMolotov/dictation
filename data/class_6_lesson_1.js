6c88e62a92449dabeae97684d451cfda02c7b736
idMolotov <idMolotov@users.noreply.github.com>
2025-10-16 18:03:10 +0700

    Create class_6_lesson_1.js
    init

---
+++ data/class_6_lesson_1.js
@@ -0,0 +1,106 @@
+let className = 'class_6';
+let lessonName = 'lesson_1';
+const sourceWords_class_6_lesson_1 = [
+        {
+            'en': ['popular'],
+            'ru': ['популярный'],
+        },
+        {
+            'en': ['real-life'],
+            'ru': ['реальная жизнь'],
+        },
+        {
+            'en': ['documentary'],
+            'ru': ['документальное кино'],
+        },
+        {
+            'en': ['reality show'],
+            'ru': ['реалити-шоу'],
+        },
+        {
+            'en': ['feature'],
+            'ru': ['особенность', 'свойство', 'характеристика', 'черта'],
+        },
+        {
+            'en': ['video footage'],
+            'ru': ['видеозапись', 'видеоматериал', 'видеосъемка', 'видеоряд'],
+        },
+        {
+            'en': ['police car'],
+            'ru': ['полицейская машина'],
+        },
+        {
+            'en': ['handheld camera'],
+            'ru': ['ручная камера'],
+        },
+        {
+            'en': ['focus on'],
+            'ru': ['сосредоточиться на', 'сфокусироваться на'],
+        },
+        {
+            'en': ['road crime'],
+            'ru': ['дорожное преступление'],
+        },
+        {
+            'en': ['catchy title'],
+            'ru': ['броский заголовок'],
+        },
+        {
+            'en': ['narrator'],
+            'ru': ['рассказчик'],
+        },
+        {
+            'en': ['draw'],
+            'ru': ['рисовать'],
+        },
+        {
+            'en': ['viewer'],
+            'ru': ['зритель'],
+        },
+        {
+            'en': ['voice-over'],
+            'ru': ['закадровый голос'],
+        },
+        {
+            'en': ['suspect'],
+            'ru': ['подозревать', 'подозреваемый'],
+        },
+        {
+            'en': ['pedestrian'],
+            'ru': ['пешеход'],
+        },
+        {
+            'en': ['injured'],
+            'ru': ['раненый'],
+        },
+        {
+            'en': ['U-turn'],
+            'ru': ['Разворот'],
+        },
+        {
+            'en': ['oncoming traffic'],
+            'ru': ['встречное движение'],
+        },
+        {
+            'en': ['shocked'],
+            'ru': ['шокирован'],
+        },
+        {
+            'en': ['speed'],
+            'ru': ['скорость'],
+        },
+        {
+            'en': ['cut sb off'],
+            'ru': ['отклбчить субтитры'],
+        },
+        {
+            'en': ['channel hop'],
+            'ru': ['переход по каналу', 'переключать каналы', 'прыгать по каналам'],
+        },
+    ];
+/**
+ * @global classLessonsList
+ */
+classLessonsList[className] = classLessonsList[className] || {};
+classLessonsList[className][lessonName] = classLessonsList[className][lessonName] || {};
+classLessonsList[className][lessonName] = sourceWords_class_4_lesson_1;
