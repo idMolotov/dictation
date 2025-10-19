49e473ca40016397e0fbe9059acbe0bbcfcba621
idMolotov <idMolotov@users.noreply.github.com>
2025-10-19 18:43:54 +0700

    Create class_6_lesson_2.js

---
+++ data/class_6_lesson_2.js
@@ -0,0 +1,54 @@
+let className = 'class_6';
+let lessonName = 'lesson_2';
+const sourceWords_class_6_lesson_2 = [
+        {
+            'en': ['leisure'],
+            'ru': ['досуг'],
+        },
+        {
+            'en': ['culture'],
+            'ru': ['культура'],
+        },
+        {
+            'en': ['attraction'],
+            'ru': ['привлекательность'],
+        },
+        {
+            'en': ['wax figure'],
+            'ru': ['восковая фигура'],
+        },
+        {
+            'en': ['ride'],
+            'ru': ['ездить'],
+        },
+        {
+            'en': ['puppet'],
+            'ru': ['марионетка'],
+        },
+        {
+            'en': ['crowd'],
+            'ru': ['толпа'],
+        },
+        {
+            'en': ['cheering'],
+            'ru': ['ободряющий'],
+        },
+        {
+            'en': ['trick'],
+            'ru': ['уловка'],
+        },
+        {
+            'en': ['performance'],
+            'ru': ['представление'],
+        },
+        {
+            'en': ['sleigh'],
+            'ru': ['сани', 'салазки', 'санки'],
+        },
+    ];
+/**
+ * @global classLessonsList
+ */
+classLessonsList[className] = classLessonsList[className] || {};
+classLessonsList[className][lessonName] = classLessonsList[className][lessonName] || {};
+classLessonsList[className][lessonName] = sourceWords_class_6_lesson_2;
