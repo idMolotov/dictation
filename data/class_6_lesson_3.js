2d4e74b0932938f84c12e91142bc3f08990ae0c4
idMolotov <idMolotov@users.noreply.github.com>
2025-11-04 14:02:20 +0700

    Update class_6_lesson_3.js

--- data/class_6_lesson_3.js
+++ data/class_6_lesson_3.js
@@ -1,73 +1,67 @@
 /**
  * @global classLessonsList
  */
-
-let className = 'class_6';
-let lessonName = 'lesson_3';
-
 Object.assign(classLessonsList, {
-    [className]: {
-        [lessonName]: [
-            {
-                'en': ['dessert'],
-                'ru': ['десерт'],
-            },
-            {
-                'en': ['juice'],
-                'ru': ['сок'],
-            },
-            {
-                'en': ['picturesque'],
-                'ru': ['живописный'],
-            },
-            {
-                'en': ['resort'],
-                'ru': ['курорт'],
-            },
-            {
-                'en': ['celebrate'],
-                'ru': ['праздновать'],
-            },
-            {
-                'en': ['attract'],
-                'ru': ['привлекать'],
-            },
-            {
-                'en': ['design'],
-                'ru': ['дизайн'],
-            },
-            {
-                'en': ['statue'],
-                'ru': ['статуя'],
-            },
-            {
-                'en': ['measure'],
-                'ru': ['мера'],
-            },
-            {
-                'en': ['citrus'],
-                'ru': ['цитрус'],
-            },
-            {
-                'en': ['creation'],
-                'ru': ['творение'],
-            },
-            {
-                'en': ['float'],
-                'ru': ['парение'],
-            },
-            {
-                'en': ['fireworks'],
-                'ru': ['фейерверк'],
-            },
-            {
-                'en': ['theme'],
-                'ru': ['тема'],
-            },
-            {
-                'en': ['imagination runs wild'],
-                'ru': ['воображение разыгралось'],
-            },
-        ],
-    },
+    'class_6_lesson_3': [
+        {
+            'en': ['dessert'],
+            'ru': ['десерт'],
+        },
+        {
+            'en': ['juice'],
+            'ru': ['сок'],
+        },
+        {
+            'en': ['picturesque'],
+            'ru': ['живописный'],
+        },
+        {
+            'en': ['resort'],
+            'ru': ['курорт'],
+        },
+        {
+            'en': ['celebrate'],
+            'ru': ['праздновать'],
+        },
+        {
+            'en': ['attract'],
+            'ru': ['привлекать'],
+        },
+        {
+            'en': ['design'],
+            'ru': ['дизайн'],
+        },
+        {
+            'en': ['statue'],
+            'ru': ['статуя'],
+        },
+        {
+            'en': ['measure'],
+            'ru': ['мера'],
+        },
+        {
+            'en': ['citrus'],
+            'ru': ['цитрус'],
+        },
+        {
+            'en': ['creation'],
+            'ru': ['творение'],
+        },
+        {
+            'en': ['float'],
+            'ru': ['парение'],
+        },
+        {
+            'en': ['fireworks'],
+            'ru': ['фейерверк'],
+        },
+        {
+            'en': ['theme'],
+            'ru': ['тема'],
+        },
+        {
+            'en': ['imagination runs wild'],
+            'ru': ['воображение разыгралось'],
+        },
+    ],
 });
