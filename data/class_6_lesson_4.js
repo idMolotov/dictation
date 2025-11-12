ea5a1495ec45dd7f1052c884c8c8aa6ce2dce9dc
idMolotov <idMolotov@users.noreply.github.com>
2025-11-12 17:49:41 +0700

    Create class_6_lesson_4.js

---
+++ data/class_6_lesson_4.js
@@ -0,0 +1,51 @@
+/**
+ * @global classLessonsList
+ */
+Object.assign(classLessonsList, {
+    'class_6_lesson_4': [
+        {
+            'en': ['celebrity'],
+            'ru': ['знаменитость'],
+        },
+        {
+            'en': ['chef'],
+            'ru': ['шеф-повар'],
+        },
+        {
+            'en': ['recipe'],
+            'ru': ['рецепт'],
+        },
+        {
+            'en': ['campaign'],
+            'ru': ['рекламная кампания'],
+        },
+        {
+            'en': ['healthy'],
+            'ru': ['здоровый'],
+        },
+        {
+            'en': ['meal'],
+            'ru': ['питание', 'еда'],
+        },
+        {
+            'en': ['habit'],
+            'ru': ['привычка'],
+        },
+        {
+            'en': ['kitchen'],
+            'ru': ['кухня'],
+        },
+        {
+            'en': ['popular'],
+            'ru': ['популярный'],
+        },
+        {
+            'en': ['reduce'],
+            'ru': ['уменьшить'],
+        },
+        {
+            'en': ['disease'],
+            'ru': ['болезнь'],
+        },
+    ],
+});
